e21292aca2789d7af7bf8ed61a33fe51d8c722c3
Jery Ramirez <alexanderamirez002@gmail.com>
2023-07-10 17:32:10 -0400

    feat: create electron app

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
 const { app, BrowserWindow, screen } = require('electron');
-const { exec, spawn } = require('child_process');
-exec("npm run front");
 
 const createApp = () => {
     const { width, height } = screen.getPrimaryDisplay().workAreaSize;
     const win = new BrowserWindow({width, height});
-    win.loadURL('http://localhost:3000');
-    win.reload();
+    win.loadURL('https://glittery-froyo-4efa38.netlify.app/');
 }
 
 app.whenReady().then( () => {
-
-    setTimeout(() => {
-        
-        createApp();
-    }, 2000);
-});
-
-app.on('quit', () => {
-    // exec("npm run front");
-    const result = exec("netstat -ano | findstr :3000");
-    exec(`taskkill /PID ${result.pid} /F`);
-    exec(`echo ${result.pid}`);
+        createApp(); 
 });
